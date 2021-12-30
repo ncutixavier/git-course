4245e5f7548a098ff7194031cb4866ce13c771b9
ncutixavier <ncuti60@gmail.com>
2021-12-31 00:54:43 +0200

    ft:add update and delete

--- js/blog.js
+++ js/blog.js
@@ -1,11 +1,17 @@
 const urlParams = new URLSearchParams(window.location.search); //?id=1
 const id = urlParams.get('id');
-
-let article = articles.find((article) => article.id == id);
-
 let blog = document.querySelector('.blog');
-if (article) {
-  let articleTemplate = `
+
+db.collection('blog')
+  .doc(id)
+  .get()
+  .then((doc) => {
+    loading.style.display = 'none';
+    console.log(doc.data());
+    let article = doc.data();
+
+    if (article) {
+      let articleTemplate = `
         <div class="blog-img">
             <img src="${article.image}" alt="" />
         </div>
@@ -13,8 +19,11 @@ if (article) {
         <p>${article.description}</p>
         <button>Leave a comment</button>
     `;
+      blog.innerHTML += articleTemplate;
+    } else {
+      blog.innerHTML = '<h1>Article has not found!</h1>';
+    }
+  });
 
-  blog.innerHTML += articleTemplate;
-} else {
-  blog.innerHTML = '<h1>Article has not found!</h1>';
-}
+// let article = articles.find((article) => article.id == id);
+
--- js/index.js
+++ js/index.js
@@ -1,48 +1,3 @@
-const articles = [
-  {
-    id: 1,
-    image: 'https://picsum.photos/300/200?image=237',
-    title: 'Animal Crossing',
-    description:
-      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. A voluptatibus totam quae delectus sint! Natus, debitis animi error ut alias dolor incidunt optio commodi odit veniam ipsum. Explicabo nam praesentium veniam! Eveniet aliquam laudantium, facilis, tempora et voluptates voluptatem labore quia eius reiciendis nostrum unde eaque, tenetur culpa nisi ut recusandae! Nobis optio odio amet, quidem impedit repudiandae provident tempore necessitatibus! Odio, debitis inventore consectetur impedit molestiae dolorem vero ducimus quibusdam ullam porro in? Alias adipisci, ad necessitatibus, tempora omnis laboriosam aliquid dolore natus aliquam dignissimos explicabo veritatis provident repudiandae, quo corporis reprehenderit dolorem voluptatem. Sunt minus in magni commodi!',
-  },
-  {
-    id: 2,
-    image: 'https://picsum.photos/300/200?image=239',
-    title: 'Animal travelling',
-    description:
-      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. A voluptatibus totam quae delectus sint! Natus, debitis animi error ut alias dolor incidunt optio commodi odit veniam ipsum. Explicabo nam praesentium veniam! Eveniet aliquam laudantium, facilis, tempora et voluptates voluptatem labore quia eius reiciendis nostrum unde eaque, tenetur culpa nisi ut recusandae! Nobis optio odio amet, quidem impedit repudiandae provident tempore necessitatibus! Odio, debitis inventore consectetur impedit molestiae dolorem vero ducimus quibusdam ullam porro in? Alias adipisci, ad necessitatibus, tempora omnis laboriosam aliquid dolore natus aliquam dignissimos explicabo veritatis provident repudiandae, quo corporis reprehenderit dolorem voluptatem. Sunt minus in magni commodi!',
-  },
-  {
-    id: 3,
-    image: 'https://picsum.photos/300/200?image=240',
-    title: 'Guest travelling',
-    description:
-      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. A voluptatibus totam quae delectus sint! Natus, debitis animi error ut alias dolor incidunt optio commodi odit veniam ipsum. Explicabo nam praesentium veniam! Eveniet aliquam laudantium, facilis, tempora et voluptates voluptatem labore quia eius reiciendis nostrum unde eaque, tenetur culpa nisi ut recusandae! Nobis optio odio amet, quidem impedit repudiandae provident tempore necessitatibus! Odio, debitis inventore consectetur impedit molestiae dolorem vero ducimus quibusdam ullam porro in? Alias adipisci, ad necessitatibus, tempora omnis laboriosam aliquid dolore natus aliquam dignissimos explicabo veritatis provident repudiandae, quo corporis reprehenderit dolorem voluptatem. Sunt minus in magni commodi!',
-  },
-  {
-    id: 4,
-    image: 'https://picsum.photos/300/200?image=295',
-    title: 'Guest travelling',
-    description:
-      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. A voluptatibus totam quae delectus sint! Natus, debitis animi error ut alias dolor incidunt optio commodi odit veniam ipsum. Explicabo nam praesentium veniam! Eveniet aliquam laudantium, facilis, tempora et voluptates voluptatem labore quia eius reiciendis nostrum unde eaque, tenetur culpa nisi ut recusandae! Nobis optio odio amet, quidem impedit repudiandae provident tempore necessitatibus! Odio, debitis inventore consectetur impedit molestiae dolorem vero ducimus quibusdam ullam porro in? Alias adipisci, ad necessitatibus, tempora omnis laboriosam aliquid dolore natus aliquam dignissimos explicabo veritatis provident repudiandae, quo corporis reprehenderit dolorem voluptatem. Sunt minus in magni commodi!',
-  },
-  {
-    id: 5,
-    image: 'https://picsum.photos/300/200?image=395',
-    title: 'Coffee Shop',
-    description:
-      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. A voluptatibus totam quae delectus sint! Natus, debitis animi error ut alias dolor incidunt optio commodi odit veniam ipsum. Explicabo nam praesentium veniam! Eveniet aliquam laudantium, facilis, tempora et voluptates voluptatem labore quia eius reiciendis nostrum unde eaque, tenetur culpa nisi ut recusandae! Nobis optio odio amet, quidem impedit repudiandae provident tempore necessitatibus! Odio, debitis inventore consectetur impedit molestiae dolorem vero ducimus quibusdam ullam porro in? Alias adipisci, ad necessitatibus, tempora omnis laboriosam aliquid dolore natus aliquam dignissimos explicabo veritatis provident repudiandae, quo corporis reprehenderit dolorem voluptatem. Sunt minus in magni commodi!',
-  },
-  {
-    id: 6,
-    image: 'https://picsum.photos/300/200?image=495',
-    title: 'Local Machine',
-    description:
-      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. A voluptatibus totam quae delectus sint! Natus, debitis animi error ut alias dolor incidunt optio commodi odit veniam ipsum. Explicabo nam praesentium veniam! Eveniet aliquam laudantium, facilis, tempora et voluptates voluptatem labore quia eius reiciendis nostrum unde eaque, tenetur culpa nisi ut recusandae! Nobis optio odio amet, quidem impedit repudiandae provident tempore necessitatibus! Odio, debitis inventore consectetur impedit molestiae dolorem vero ducimus quibusdam ullam porro in? Alias adipisci, ad necessitatibus, tempora omnis laboriosam aliquid dolore natus aliquam dignissimos explicabo veritatis provident repudiandae, quo corporis reprehenderit dolorem voluptatem. Sunt minus in magni commodi!',
-  },
-];
-
 const formContainer = document.querySelector('.form-container');
 const createBtn = document.querySelector('.create-btn');
 const closeModal = document.querySelector('.close-modal');
@@ -53,15 +8,18 @@ const image = document.querySelector('#image');
 const addArticleBtn = document.querySelector('.add-article-btn');
 const alert = document.querySelector('.alert');
 const loading = document.querySelector('.loading');
+const updateArticleBtn = document.querySelector('.update-article-btn');
 
 function displayArticles(articles) {
   let blog = '';
   for (let i = 0; i < articles.length; i++) {
     let template = `
-        <a class="article" href="blog.html?id=${articles[i].id}">
+        <a class="article" href="blog.html?id=${articles[i].id}" data-id=${
+      articles[i].id
+    } >
             <div>
                 <div class="article-img">
-                    <img src="${articles[i].image}" alt="" srcset="">
+                    <img src="${articles[i].image}" alt="image">
                 </div>
                 <h3>${articles[i].title}</h3>
                 <p>${articles[i].description.substring(0, 120)}...</p>
@@ -82,12 +40,14 @@ function displayArticles(articles) {
 }
 
 const articlesContainer = document.querySelector('.articles');
-// articlesContainer.innerHTML = displayArticles(articles);
 
+//open modal when create article button is clicked
 createBtn.addEventListener('click', () => {
+  updateArticleBtn.style.display = 'none';
   formContainer.classList.toggle('open-modal');
 });
 
+//close modal when cancel button is clicked
 closeModal.addEventListener('click', () => {
   formContainer.classList.toggle('open-modal');
 });
@@ -95,39 +55,140 @@ closeModal.addEventListener('click', () => {
 addArticleForm.addEventListener('submit', (e) => {
   e.preventDefault();
 
-  const article = {
-    title: title.value,
-    description: description.value,
-    image: image.value,
-  };
-  console.log(article);
+  //check if all fields are filled
+  if (!title.value || !description.value || !image.value) {
+    alert.style.display = 'block';
+    alert.style.backgroundColor = '#f44336';
+    alert.style.color = '#fff';
+    alert.innerHTML = `Please fill all fields`;
+  } else {
+    const article = {
+      title: title.value,
+      description: description.value,
+      image: image.value,
+    };
+    console.log(article);
 
-  addArticleBtn.innerHTML += '<i class="fas fa-spinner fa-spin"></i>';
-  addArticleBtn.setAttribute('disabled', 'disabled');
+    addArticleBtn.innerHTML += '<i class="fas fa-spinner fa-spin"></i>';
+    addArticleBtn.setAttribute('disabled', 'disabled');
 
-  db.collection('blog')
-    .add(article)
-    .then(() => {
-      alert.style.display = 'block';
-      alert.innerHTML = `Article added successfully`;
-      addArticleBtn.innerHTML = 'Add Article';
-      addArticleBtn.removeAttribute('disabled');
+    //add article to firebase
+    db.collection('blog')
+      .add(article)
+      .then(() => {
+        alert.style.display = 'block';
+        alert.innerHTML = `Article added successfully`;
+        addArticleBtn.innerHTML = 'Add Article';
+        addArticleBtn.removeAttribute('disabled');
 
-      setTimeout(() => {
-        formContainer.classList.toggle('open-modal');
-        window.location.reload();
-      }, 1000);
-    });
+        //close modal when successful added
+        setTimeout(() => {
+          formContainer.classList.toggle('open-modal');
+          window.location.reload();
+        }, 1000);
+      });
+  }
 });
 
+//Display articles
 db.collection('blog')
   .get()
   .then((snapshot) => {
     let articles = [];
     snapshot.docs.forEach((doc) => {
-      console.log(doc.data());
-      articles.push(doc.data());
+      articles.push({
+        id: doc.id,
+        title: doc.data().title,
+        description: doc.data().description,
+        image: doc.data().image,
+      });
     });
+    console.log('ART::', articles);
     loading.style.display = 'none';
     articlesContainer.innerHTML = displayArticles(articles);
+    console.log(articlesContainer);
+
+    const articlesLinks = document.querySelectorAll('.article');
+    const editBtns = document.querySelectorAll('.edit-btn');
+    const deleteBtns = document.querySelectorAll('.delete-btn');
+
+    // articlesLinks.forEach((article) => {
+    //   article.addEventListener('click', (e) => {
+    //     e.preventDefault();
+    //     console.log(article.getAttribute('data-id'));
+    //     const id = article.getAttribute('data-id');
+    //     window.location.href = `blog.html?id=${id}`;
+    //   });
+    // });
+
+    deleteBtns.forEach((btn) => {
+      //btn to delete article
+      btn.addEventListener('click', (e) => {
+        btn.innerHTML = '<i class="fas fa-spinner fa-spin"></i>';
+        e.preventDefault();
+        const id =
+          btn.parentElement.parentElement.parentElement.getAttribute('data-id');
+        db.collection('blog')
+          .doc(id)
+          .delete()
+          .then(() => {
+            btn.innerHTML = '<i class="far fa-trash-alt"></i>';
+            window.location.reload();
+          });
+      });
+    });
+
+    editBtns.forEach((editBtn) => {
+      editBtn.addEventListener('click', (e) => {
+        addArticleBtn.style.display = 'none';
+        updateArticleBtn.style.display = 'block';
+        editBtn.innerHTML = '<i class="fas fa-spinner fa-spin"></i>';
+
+        e.preventDefault();
+        const id =
+          editBtn.parentElement.parentElement.parentElement.getAttribute(
+            'data-id'
+          );
+
+        //get the article details and fill data in the form
+        db.collection('blog')
+          .doc(id)
+          .get()
+          .then((doc) => {
+            title.value = doc.data().title;
+            description.value = doc.data().description;
+            image.value = doc.data().image;
+            formContainer.classList.toggle('open-modal');
+            editBtn.innerHTML = '<i class="far fa-edit"></i>';
+            console.log(doc.data());
+
+            //update article
+            updateArticleBtn.addEventListener('click', (e) => {
+              updateArticleBtn.innerHTML +=
+                '<i class="fas fa-spinner fa-spin"></i>';
+
+              e.preventDefault();
+              const article = {
+                title: title.value,
+                description: description.value,
+                image: image.value,
+              };
+              console.log(article);
+              db.collection('blog')
+                .doc(id)
+                .update(article)
+                .then(() => {
+                  alert.style.display = 'block';
+                  alert.innerHTML = `Article updated successfully`;
+                  updateArticleBtn.innerHTML = 'Update Article';
+
+                  setTimeout(() => {
+                    formContainer.classList.toggle('open-modal');
+                    window.location.reload();
+                  }, 1000);
+                });
+            });
+          });
+      });
+    });
   });
