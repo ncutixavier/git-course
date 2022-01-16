687e08aa2da9f89a6139e672af4598a8e27c8a7a
ncutixavier <ncuti60@gmail.com>
2022-01-17 01:01:02 +0200

    crud with localstorage

--- js/blog.js
+++ js/blog.js
@@ -1,29 +1,83 @@
 const urlParams = new URLSearchParams(window.location.search); //?id=1
-const id = urlParams.get('id');
-let blog = document.querySelector('.blog');
+const id = urlParams.get("id");
+let blog = document.querySelector(".blog");
+const loading = document.querySelector(".loading");
+const formContainer = document.querySelector(".form-container");
+const addCommentBtn = document.querySelector(".add-comment-btn");
+const name = document.querySelector("#name");
+const comment = document.querySelector("#comment");
+const alertInfo = document.querySelector(".alert");
+const addCommentForm = document.querySelector(".add-comment-form");
 
-db.collection('blog')
-  .doc(id)
-  .get()
-  .then((doc) => {
-    loading.style.display = 'none';
-    console.log(doc.data());
-    let article = doc.data();
-
+(function () {
+  //get articles from localStorage
+  let articles = JSON.parse(localStorage.getItem("articles"));
+  //display article
+  if (articles === null || articles.length === 0) {
+    loading.style.display = "none";
+    blog.innerHTML = "<h2>Article has not found!</h2>";
+  } else {
+    loading.style.display = "none";
+    let article = articles.find((article) => article.id == id);
     if (article) {
+      let comments = ""
+      for (let i = 0; i < article.comments.length; i++) { 
+        comments += `
+          <div class="comment">
+            <h3>${article.comments[i].name}</h3>
+            <p>${article.comments[i].comment}</p>
+          </div>
+        `;
+      }
       let articleTemplate = `
         <div class="blog-img">
             <img src="${article.image}" alt="" />
         </div>
         <h1>${article.title}</h1>
         <p>${article.description}</p>
-        <button>Leave a comment</button>
+        <button class="leave-comment-btn">Leave a comment</button>
+        <div class="comments-container">
+          <div class="comments-header">
+            <h1>Comments</h1>
+            <hr />
+          </div>
+          <div class="comments">${comments}</div>
+        </div>
     `;
       blog.innerHTML += articleTemplate;
+      const leaveCommentBtn = document.querySelector(".leave-comment-btn");
+      const closeModal = document.querySelector(".close-modal");
+      leaveCommentBtn.addEventListener("click", () => {
+        formContainer.classList.toggle("open-modal");
+        window.scrollTo(0, 0, {
+          behavior: "smooth",
+        });
+      });
+
+      addCommentForm.addEventListener("submit", (e) => {
+        e.preventDefault();
+        if (!name.value || !comment.value) {
+          alertInfo.style.display = "block";
+          alertInfo.style.backgroundColor = "#f44336";
+          alertInfo.style.color = "#fff";
+          alertInfo.innerHTML = `Please fill all fields`;
+        } else {
+          let articles = JSON.parse(localStorage.getItem("articles"));
+          let article = articles.find((article) => article.id == id);
+          article.comments.push({
+            name: name.value,
+            comment: comment.value,
+          });
+          localStorage.setItem("articles", JSON.stringify(articles));
+          window.location.reload(); 
+        }
+       });
+
+      closeModal.addEventListener("click", () => {
+        formContainer.classList.toggle("open-modal");
+      });
     } else {
-      blog.innerHTML = '<h1>Article has not found!</h1>';
+      blog.innerHTML = "<h1>Article has not found!</h1>";
     }
-  });
-
-// let article = articles.find((article) => article.id == id);
-
+  }
+})();
--- js/index.js
+++ js/index.js
@@ -1,66 +1,67 @@
-const formContainer = document.querySelector('.form-container');
-const createBtn = document.querySelector('.create-btn');
-const closeModal = document.querySelector('.close-modal');
-const addArticleForm = document.querySelector('.add-article-form');
-const title = document.querySelector('#title');
-const description = document.querySelector('#content');
-const image = document.querySelector('#image');
-const addArticleBtn = document.querySelector('.add-article-btn');
-const alert = document.querySelector('.alert');
-const loading = document.querySelector('.loading');
-const updateArticleBtn = document.querySelector('.update-article-btn');
+const formContainer = document.querySelector(".form-container");
+const createBtn = document.querySelector(".create-btn");
+const closeModal = document.querySelector(".close-modal");
+const addArticleForm = document.querySelector(".add-article-form");
+const title = document.querySelector("#title");
+const description = document.querySelector("#content");
+const image = document.querySelector("#image");
+const addArticleBtn = document.querySelector(".add-article-btn");
+const infoAlert = document.querySelector(".alert");
+const loading = document.querySelector(".loading");
+const updateArticleBtn = document.querySelector(".update-article-btn");
 
+//function to display all articles
 function displayArticles(articles) {
-  let blog = '';
+  let blog = "";
   for (let i = 0; i < articles.length; i++) {
     let template = `
-        <a class="article" href="blog.html?id=${articles[i].id}" data-id=${
-      articles[i].id
-    } >
-            <div>
-                <div class="article-img">
-                    <img src="${articles[i].image}" alt="image">
-                </div>
-                <h3>${articles[i].title}</h3>
-                <p>${articles[i].description.substring(0, 120)}...</p>
-                <div class="action-btn">
-                  <button type="submit" class="edit-btn">
-                    <i class="far fa-edit"></i>
-                  </button>
-                  <button type="submit" class="delete-btn">
-                    <i class="far fa-trash-alt"></i>
-                  </button>
-                </div>
-            </div>
-        </a>
+          <div class="article" data-id=${articles[i].id}>
+              <div class="article-img">
+                  <img src="${articles[i].image}" alt="image">
+              </div>
+              <h3>${articles[i].title}</h3>
+              <p>${articles[i].description.substring(0, 120)}...</p>
+              <div class="action-btn">
+                <button type="submit" class="btn edit-btn">
+                  <i class="far fa-edit"></i>
+                </button>
+                <button type="submit" class="btn view-btn">
+                  <i class="far fa-eye"></i>
+                </button>
+                <button type="submit" class="btn delete-btn">
+                  <i class="far fa-trash-alt"></i>
+                </button>
+              </div>
+          </div>
         `;
     blog += template;
   }
   return blog;
 }
 
-const articlesContainer = document.querySelector('.articles');
+const articlesContainer = document.querySelector(".articles");
 
 //open modal when create article button is clicked
-createBtn.addEventListener('click', () => {
-  updateArticleBtn.style.display = 'none';
-  formContainer.classList.toggle('open-modal');
+createBtn.addEventListener("click", () => {
+  updateArticleBtn.style.display = "none";
+  formContainer.classList.toggle("open-modal");
 });
 
 //close modal when cancel button is clicked
-closeModal.addEventListener('click', () => {
-  formContainer.classList.toggle('open-modal');
+closeModal.addEventListener("click", () => {
+  formContainer.classList.toggle("open-modal");
 });
 
-addArticleForm.addEventListener('submit', (e) => {
+//add article when add article button is clicked
+addArticleForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
   //check if all fields are filled
   if (!title.value || !description.value || !image.value) {
-    alert.style.display = 'block';
-    alert.style.backgroundColor = '#f44336';
-    alert.style.color = '#fff';
-    alert.innerHTML = `Please fill all fields`;
+    infoAlert.style.display = "block";
+    infoAlert.style.backgroundColor = "#f44336";
+    infoAlert.style.color = "#fff";
+    infoAlert.innerHTML = `Please fill all fields`;
   } else {
     const article = {
       title: title.value,
@@ -70,125 +71,116 @@ addArticleForm.addEventListener('submit', (e) => {
     console.log(article);
 
     addArticleBtn.innerHTML += '<i class="fas fa-spinner fa-spin"></i>';
-    addArticleBtn.setAttribute('disabled', 'disabled');
+    addArticleBtn.setAttribute("disabled", "disabled");
 
-    //add article to firebase
-    db.collection('blog')
-      .add(article)
-      .then(() => {
-        alert.style.display = 'block';
-        alert.innerHTML = `Article added successfully`;
-        addArticleBtn.innerHTML = 'Add Article';
-        addArticleBtn.removeAttribute('disabled');
-
-        //close modal when successful added
-        setTimeout(() => {
-          formContainer.classList.toggle('open-modal');
-          window.location.reload();
-        }, 1000);
+    //add article to localStorage
+    if (localStorage.getItem("articles") === null) {
+      let articles = [];
+      articles.push({
+        id: 1,
+        ...article,
+        comments: [],
       });
+      localStorage.setItem("articles", JSON.stringify(articles));
+      addArticleBtn.innerHTML = 'Added <i class="fas fa-check"></i>';
+      addArticleBtn.setAttribute("disabled", "disabled");
+      addArticleForm.reset();
+      setTimeout(() => {
+        addArticleBtn.innerHTML = "Add Article";
+        addArticleBtn.removeAttribute("disabled");
+        formContainer.classList.toggle("open-modal");
+        window.location.reload();
+      }, 2000);
+    } else {
+      let articles = JSON.parse(localStorage.getItem("articles"));
+      articles.push({
+        id: articles.length + 1,
+        ...article,
+        comments: [],
+      });
+      localStorage.setItem("articles", JSON.stringify(articles));
+      addArticleBtn.innerHTML = 'Added <i class="fas fa-check"></i>';
+      addArticleBtn.setAttribute("disabled", "disabled");
+      addArticleForm.reset();
+      setTimeout(() => {
+        addArticleBtn.innerHTML = "Add Article";
+        addArticleBtn.removeAttribute("disabled");
+        formContainer.classList.toggle("open-modal");
+        window.location.reload();
+      }, 500);
+    }
   }
 });
 
-//Display articles
-db.collection('blog')
-  .get()
-  .then((snapshot) => {
-    let articles = [];
-    snapshot.docs.forEach((doc) => {
-      articles.push({
-        id: doc.id,
-        title: doc.data().title,
-        description: doc.data().description,
-        image: doc.data().image,
-      });
-    });
-    console.log('ART::', articles);
-    loading.style.display = 'none';
+(function () {
+  //get articles from localStorage
+  let articles = JSON.parse(localStorage.getItem("articles"));
+
+  //display articles
+  if (articles === null || articles.length === 0) {
+    loading.style.display = "none";
+    articlesContainer.innerHTML = `<h2>No articles yet</h2>`;
+    articlesContainer.classList.add("blog-container");
+  } else {
     articlesContainer.innerHTML = displayArticles(articles);
-    console.log(articlesContainer);
+    loading.style.display = "none";
 
-    const articlesLinks = document.querySelectorAll('.article');
-    const editBtns = document.querySelectorAll('.edit-btn');
-    const deleteBtns = document.querySelectorAll('.delete-btn');
+    const allArticles = document.querySelectorAll(".article");
+    const editBtns = document.querySelectorAll(".edit-btn");
+    const viewBtns = document.querySelectorAll(".view-btn");
+    const deleteBtns = document.querySelectorAll(".delete-btn");
 
-    // articlesLinks.forEach((article) => {
-    //   article.addEventListener('click', (e) => {
-    //     e.preventDefault();
-    //     console.log(article.getAttribute('data-id'));
-    //     const id = article.getAttribute('data-id');
-    //     window.location.href = `blog.html?id=${id}`;
-    //   });
-    // });
-
-    deleteBtns.forEach((btn) => {
-      //btn to delete article
-      btn.addEventListener('click', (e) => {
-        btn.innerHTML = '<i class="fas fa-spinner fa-spin"></i>';
-        e.preventDefault();
-        const id =
-          btn.parentElement.parentElement.parentElement.getAttribute('data-id');
-        db.collection('blog')
-          .doc(id)
-          .delete()
-          .then(() => {
-            btn.innerHTML = '<i class="far fa-trash-alt"></i>';
-            window.location.reload();
-          });
+    //View article
+    viewBtns.forEach((btn) => {
+      btn.addEventListener("click", (e) => {
+        let articleId = btn.parentNode.parentNode.getAttribute("data-id");
+        console.log(articleId);
+        window.location.href = `blog.html?id=${articleId}`;
       });
     });
 
-    editBtns.forEach((editBtn) => {
-      editBtn.addEventListener('click', (e) => {
-        addArticleBtn.style.display = 'none';
-        updateArticleBtn.style.display = 'block';
-        editBtn.innerHTML = '<i class="fas fa-spinner fa-spin"></i>';
-
-        e.preventDefault();
-        const id =
-          editBtn.parentElement.parentElement.parentElement.getAttribute(
-            'data-id'
-          );
-
-        //get the article details and fill data in the form
-        db.collection('blog')
-          .doc(id)
-          .get()
-          .then((doc) => {
-            title.value = doc.data().title;
-            description.value = doc.data().description;
-            image.value = doc.data().image;
-            formContainer.classList.toggle('open-modal');
-            editBtn.innerHTML = '<i class="far fa-edit"></i>';
-            console.log(doc.data());
-
-            //update article
-            updateArticleBtn.addEventListener('click', (e) => {
-              updateArticleBtn.innerHTML +=
-                '<i class="fas fa-spinner fa-spin"></i>';
-
-              e.preventDefault();
-              const article = {
-                title: title.value,
-                description: description.value,
-                image: image.value,
-              };
-              console.log(article);
-              db.collection('blog')
-                .doc(id)
-                .update(article)
-                .then(() => {
-                  alert.style.display = 'block';
-                  alert.innerHTML = `Article updated successfully`;
-                  updateArticleBtn.innerHTML = 'Update Article';
-
-                  setTimeout(() => {
-                    formContainer.classList.toggle('open-modal');
-                    window.location.reload();
-                  }, 1000);
-                });
-            });
-          });
+    //delete article
+    deleteBtns.forEach((btn) => { 
+      btn.addEventListener("click", (e) => {
+        let articleId = btn.parentNode.parentNode.getAttribute("data-id");
+        console.log(articleId);
+        let articles = JSON.parse(localStorage.getItem("articles"));
+        let filteredArticles = articles.filter((article) => article.id !== Number(articleId));
+        console.log(filteredArticles);
+        localStorage.setItem("articles", JSON.stringify(filteredArticles));
+        window.location.reload();
       });
     });
-  });
+
+    //Edit article
+    editBtns.forEach((btn) => { 
+      btn.addEventListener("click", (e) => {
+        let articleId = btn.parentNode.parentNode.getAttribute("data-id");
+        console.log(articleId);
+        addArticleBtn.style.display = "none";
+        updateArticleBtn.style.display = "block";
+        updateArticleBtn.setAttribute("data-id", articleId);
+        formContainer.classList.toggle("open-modal");
+
+        let articles = JSON.parse(localStorage.getItem("articles"));
+        let article = articles.find((article) => article.id === Number(articleId));
+        console.log(article);
+        title.value = article.title;
+        description.value = article.description;
+        image.value = article.image;
+
+        updateArticleBtn.addEventListener("click", (e) => { 
+          e.preventDefault();
+          let articles = JSON.parse(localStorage.getItem("articles"));
+          let article = articles.find((article) => article.id === Number(articleId));
+          article.title = title.value;
+          article.description = description.value;
+          article.image = image.value;
+          article.comments = article.comments;
+          localStorage.setItem("articles", JSON.stringify(articles));
+          window.location.reload();
+        });
+      });
+    });
+  }
+})();
